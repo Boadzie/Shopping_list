954706a5d32d23182a0232dd42e08dcafa72efc8
Boadzie Daniel <boadziedaniel43@gmail.com>
2018-05-17 10:03:21 +0000

    Added drink and ingredients scaffold and routes

--- config/application.rb
+++ config/application.rb
@@ -30,6 +30,8 @@ module ShoppingList
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
 
+    config.app_generators.scaffold_controller = :scaffold_controller
+
     # Middleware for ActiveAdmin
     config.middleware.use Rack::MethodOverride
     config.middleware.use ActionDispatch::Flash
